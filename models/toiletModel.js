206ea189822a765fd479eb40284cc1910a5629b4
Akeops <lalanne.andoni1@gmail.com>
2024-04-09 09:07:59 +0200

    Structure des tables terminées

---
+++ models/toiletModel.js
@@ -0,0 +1,46 @@
+const { db } = require("./db");
+
+const { DataTypes } = require("sequelize");
+
+const Toilet = db.define(
+  "Toilet",
+  {
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    adress: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    borough: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hours: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    disableAcs: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    babyAccess: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    geoPointLon: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    geoPointLat: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  },
+  {
+    // timestamps: false,
+  },
+);
+
+module.exports = { Toilet };
---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const { db } = require("./db");
+
+const { DataTypes } = require("sequelize");
+
+const User = db.define(
+  "User",
+  {
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    // timestamps: false,
+  },
+);
+
+module.exports = { User };
