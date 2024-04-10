afa4eb1fcbbdca5def676b9502c65b7aa05b5023
ArthurMns <arthur.mounissens@gmail.com>
2024-04-10 18:52:57 +0200

    Update manager.js

--- api/manager.js
+++ api/manager.js
@@ -9,6 +9,12 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/toilets", toiletRouter);
 
