4084a351ae13d0aaba5e738ae92809e42e630964
UserWhoCode <victor.mauget@efrei.net>
2024-04-10 18:36:24 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBarConnecte";
+import ToiletCard from "./components/ToiletCard";
+import Help from "./components/Help";
+import LegalMention from "./components/LegalMention";
 
 function App() {
   return (
