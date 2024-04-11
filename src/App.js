1c1b52de404bdfaadc5aae62ab4f3086e688f969
C4rpz <jules.grosjean@efrei.net>
2024-04-11 10:01:18 +0200

    DEButMap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBarConnecte";
-import MapComponent from './components/MapComponent';
+import MapComponent from "./components/MapComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <NavBar />
-      </header>
-      <main>
-        <MapComponent />
-      </main>
-    </div>
-  );
+	return (
+		<div className="App">
+			
+			<main>
+				<NavBar />
+				<MapComponent />
+			</main>
+		</div>
+	);
 }
 
 export default App;
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,33 +1,14 @@
-import React, { Component } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
-import '../styles/map.css';  
+import React, { useRef, useEffect, useState } from "react";
 
-const MAPBOX_ACCESS_TOKEN = "map token";
+import mapboxgl from "!mapbox-gl";
 
-class Map extends Component {
-  state = {
-    viewport: {
-      latitude: 48.8566,
-      longitude: 2.3488,
-      zoom: 12,
-    },
-  };
+mapboxgl.accessToken =
+	"pk.eyJ1Ijoic3lsdmFpbmdhbHRpZXIiLCJhIjoiY2tsZ3JoZ3kyMWV3OTJ3cDdrcjM0azh0eiJ9.zH81EkDqnNnXFigXe1f7PQ";
 
-  render() {
-    const { viewport } = this.state;
-
-    return (
-      <div className="map-container">  
-        <ReactMapGL
-          {...viewport}
-          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
-          onViewportChange={(viewport) => this.setState({ viewport })}
-        >
-          <Marker latitude={48.8566} longitude={2.3488} />
-        </ReactMapGL>
-      </div>
-    );
-  }
+export default function App() {
+	const mapContainer = useRef(null);
+	const map = useRef(null);
+	const [lng, setLng] = useState(2.3488);
+	const [lat, setLat] = useState(48.8566);
+	const [zoom, setZoom] = useState(12);
 }
-
-export default Map;
