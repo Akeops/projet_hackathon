a2a3cd800b1bfeb2442216c539a8b6532bc809bf
C4rpz <jules.grosjean@efrei.net>
2024-04-11 18:03:18 +0200

    commit MAJ

--- api/manager.js
+++ api/manager.js
@@ -5,9 +5,18 @@ const userRouter = require("./routes/userRoute");
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/toilets", toiletRouter);
 
