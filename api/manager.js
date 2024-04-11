d77082acf31e5a28e7aa56e4d5ebf99a03299c22
Andoni Lalanne-Berdouticq <lalanne.andoni1@gmail.com>
2024-04-11 19:05:48 +0200

    update branch

--- api/manager.js
+++ api/manager.js
@@ -5,7 +5,15 @@ const userRouter = require("./routes/userRoute");
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+const PORT = 3001;
 
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
