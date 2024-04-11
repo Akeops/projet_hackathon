8477b6aeb3973495c329163c11865fecf6be04b3
UserWhoCode <victor.mauget@efrei.net>
2024-04-11 15:42:59 +0200

    update

--- src/components/LegalMention.js
+++ src/components/LegalMention.js
@@ -5,18 +5,18 @@ const LegalMention = () => {
     <div className="legal-mentions">
       <h1>Mentions Légales</h1>
       <h2>Identification de l'éditeur du site</h2>
-      <p>Nom de la société : </p>
-      <p>Adresse : </p>
+      <p>Nom de la société : Pause Parisienne</p>
+      <p>Adresse : 83 chemin du campupus</p>
       <p>Téléphone : 01 23 45 67 89</p>
-      <p>Email : </p>
+      <p>Email : TuturLaVoiture@efrei.net</p>
 
       <h2>Hébergement</h2>
-      <p>Ce site est hébergé par </p>
-      <p>Adresse : </p>
-      <p>Téléphone : </p>
+      <p>Ce site est hébergé par ANDONI</p>
+      <p>Adresse : 83 chemin du campupus</p>
+      <p>Téléphone : 01 23 45 67 89</p>
 
       <h2>Propriété intellectuelle</h2>
-      <p>Le contenu de ce site, incluant les textes, images et graphiques, appartient à XYZ. Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du site, sans l’autorisation de XYZ est prohibée et pourra entraîner des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et/ou le Code civil.</p>
+      <p>Le contenu de ce site, incluant les textes, images et graphiques, appartient à Jules. Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du site, sans l’autorisation de Victor est prohibée et pourra entraîner des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et/ou le Code civil.</p>
     </div>
   );
 };
