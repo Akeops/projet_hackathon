8abc2fa0296bdd703e31c0b731ea80b8905e647c
ArthurMns <arthur.mounissens@gmail.com>
2024-04-11 10:40:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import NavBar from "./components/NavBarConnecte";
 import MapComponent from "./components/MapComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <NavBar />
-      </header>
-      <main>
-        <MapComponent />
-      </main>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<NavBar />
+			</header>
+			<main>
+				<MapComponent />
+			</main>
+		</div>
+	);
 }
 
 export default App;
