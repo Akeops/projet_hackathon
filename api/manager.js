28c8043a95fc30a2232c705d1f76e95e32e063fe
ArthurMns <arthur.mounissens@gmail.com>
2024-04-12 11:37:45 +0200

    Update manager.js

--- api/manager.js
+++ api/manager.js
@@ -1,18 +1,18 @@
 const { db } = require("./models/db");
 const toiletRouter = require("./routes/toiletRoute");
 const userRouter = require("./routes/userRoute");
-const cors = require("cors");
+// const cors = require("cors");
 
 const express = require("express");
 
 const app = express();
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  );
-  next();
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
 });
 const PORT = 3001;
 
@@ -38,28 +38,28 @@ app.use("/api/v1/toilets", toiletRouter);
 
 // db.sync({force: true}).then(async () => {
 db.sync().then(async () => {
-  // insertData().then((data) => {
-  //   data.forEach((element) => {
-  //     toiletService.createToilet({
-  //       type: element.type,
-  //       adress: element.adresse,
-  //       borough: element.arrondissement,
-  //       hours: element.horaire,
-  //       disableAcs: element.acces_pmr,
-  //       babyAccess: element.relais_bebe,
-  //       geoPointLon: element.geo_point_2d.lon,
-  //       geoPointLat: element.geo_point_2d.lat,
-  //     });
-  //   });
-  // });
-  // userService.createUser({
-  // 	nickname: "John",
-  // 	email: "email@mail.com",
-  // 	password: "root",
-  // });
-  app.listen(PORT, () => {
-    console.log(
-      `Serveur running, triple moooooonstre on http://localhost:${PORT}`,
-    );
-  });
+	// insertData().then((data) => {
+	//   data.forEach((element) => {
+	//     toiletService.createToilet({
+	//       type: element.type,
+	//       adress: element.adresse,
+	//       borough: element.arrondissement,
+	//       hours: element.horaire,
+	//       disableAcs: element.acces_pmr,
+	//       babyAccess: element.relais_bebe,
+	//       geoPointLon: element.geo_point_2d.lon,
+	//       geoPointLat: element.geo_point_2d.lat,
+	//     });
+	//   });
+	// });
+	// userService.createUser({
+	// 	nickname: "John",
+	// 	email: "email@mail.com",
+	// 	password: "root",
+	// });
+	app.listen(PORT, () => {
+		console.log(
+			`Serveur running, triple moooooonstre on http://localhost:${PORT}`
+		);
+	});
 });
