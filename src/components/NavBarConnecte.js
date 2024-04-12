89c8dd1a2fc67dd29367154847b073bd9c7fea3e
Andoni Lalanne-Berdouticq <lalanne.andoni1@gmail.com>
2024-04-12 11:34:59 +0200

    update
    ipdaf

--- src/components/NavBarConnecte.js
+++ src/components/NavBarConnecte.js
@@ -4,115 +4,104 @@ import "../styles/NavBarConnecte.css";
 import UserPage from "./UserPage";
 
 const NavBar = () => {
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
-	const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-	const toggleButtonRef = useRef(null);
-	const dropdownRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const toggleButtonRef = useRef(null);
+  const dropdownRef = useRef(null);
 
-	useEffect(() => {
-		const handleClickOutside = (event) => {
-			if (
-				dropdownRef.current &&
-				!dropdownRef.current.contains(event.target)
-			) {
-				setIsMenuOpen(false);
-				setIsProfileMenuOpen(false);
-			}
-		};
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+        setIsProfileMenuOpen(false);
+      }
+    };
 
-		document.addEventListener("mousedown", handleClickOutside);
-		return () => {
-			document.removeEventListener("mousedown", handleClickOutside);
-		};
-	}, []);
-	const toggleMenu = () => {
-		setIsMenuOpen(!isMenuOpen);
-	};
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-	const toggleProfileMenu = () => {
-		setIsProfileMenuOpen(!isProfileMenuOpen);
-	};
+  const toggleProfileMenu = () => {
+    setIsProfileMenuOpen(!isProfileMenuOpen);
+  };
 
-	return (
-		<nav className="nav">
-			<div className="navSec1">
-				<div>
-					<div className="divLink" onClick={toggleMenu}>
-						<img
-							src="./images/menu.png"
-							alt="menu"
-							className="img-menu"
-						/>
-					</div>
-					{isMenuOpen && (
-						<div className="dropdown-menu" ref={dropdownRef}>
-							<div className="dropDownTop">
-								<div className="divLink">
-									<p>Menu</p>
-								</div>
-								<Link to="/" className="elementDropDown">
-									Accueil
-								</Link>
-								<Link
-									to="/annuaire"
-									className="elementDropDown"
-									onClick={toggleMenu}>
-									Toilettes de Paris
-								</Link>
-							</div>
+  return (
+    <nav className="nav">
+      <div className="navSec1">
+        <div>
+          <div className="divLink" onClick={toggleMenu}>
+            <img src="./images/menu.png" alt="menu" className="img-menu" />
+          </div>
+          {isMenuOpen && (
+            <div className="dropdown-menu" ref={dropdownRef}>
+              <div className="dropDownTop">
+                <div className="divLink">
+                  <p>Menu</p>
+                </div>
+                <Link to="/" className="elementDropDown">
+                  Accueil
+                </Link>
+                <Link
+                  to="/annuaire"
+                  className="elementDropDown"
+                  onClick={toggleMenu}
+                >
+                  Toilettes de Paris
+                </Link>
+              </div>
 
-							<div className="dropDownBottom">
-								<Link
-									to="/contact"
-									className="elementDropDown2">
-									Nous contacter
-								</Link>
-								<Link to="/aide" className="elementDropDown2">
-									Aides
-								</Link>
-								<Link
-									to="/mentionLegale"
-									className="elementDropDown2">
-									Mentions Légales
-								</Link>
-							</div>
-						</div>
-					)}
-				</div>
+              <div className="dropDownBottom">
+                <Link to="/contact" className="elementDropDown2">
+                  Nous contacter
+                </Link>
+                <Link to="/aide" className="elementDropDown2">
+                  Aides
+                </Link>
+                <Link to="/mentionLegale" className="elementDropDown2">
+                  Mentions Légales
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
 
-				<Link to="/" className="Titre-h1">
-					Pause Parisienne
-				</Link>
-			</div>
+        <Link to="/" className="Titre-h1">
+          Pause Parisienne
+        </Link>
+      </div>
 
-			<div className="navSec2">
-				<div className="navSec2-1">
-					<div className=""></div>
-				</div>
-				<div className="navSec2">
-					<div className="divLink" onClick={toggleProfileMenu}>
-						<img
-							src="../images/icon-profil.png"
-							alt="profil"
-							className="img-profil"
-						/>
-					</div>
-					{isProfileMenuOpen && (
-						<div className="dropdown-menu-right" ref={dropdownRef}>
-							{/* Éléments du menu du profil */}
-							<Link to="/inscription" className="elementDropDown">
-								Inscription
-							</Link>
-							<Link to="/connection" className="elementDropDown">
-								Connection
-							</Link>
-							<UserPage />
-						</div>
-					)}
-				</div>
-			</div>
-		</nav>
-	);
+      <div className="navSec2">
+        <div className="navSec2-1">
+          <div className=""></div>
+        </div>
+        <div className="navSec2">
+          <div className="divLink" onClick={toggleProfileMenu}>
+            <img
+              src="../images/icon-profil.png"
+              alt="profil"
+              className="img-profil"
+            />
+          </div>
+          {isProfileMenuOpen && (
+            <div className="dropdown-menu-right" ref={dropdownRef}>
+              {/* Éléments du menu du profil */}
+              <Link to="/inscription" className="elementDropDown">
+                Inscription
+              </Link>
+              <Link to="/connection" className="elementDropDown">
+                Connection
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
