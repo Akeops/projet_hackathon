f230ce784615a995294d8df91c7ebe6b7b4304a5
Andoni Lalanne-Berdouticq <lalanne.andoni1@gmail.com>
2024-04-12 11:05:30 +0200

    update
    update

--- src/components/ConnectionForm.js
+++ src/components/ConnectionForm.js
@@ -1,60 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/UserComponent.css";
-import { Link } from "react-router-dom";
 
 const ConnectionForm = () => {
-	return (
-		<div className="container">
-			<div className="form-wrapper">
-				<div>
-					<h2 className="form-element">Connectez-vous</h2>
-				</div>
-				<form action="#" method="POST">
-					<div className="form-element">
-						<label htmlFor="email">Adresse Mail</label>
-						<div>
-							<input
-								id="email"
-								name="email"
-								type="email"
-								autoComplete="email"
-								required
-							/>
-						</div>
-					</div>
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  let navigate = useNavigate();
 
-					<div className="form-element">
-						<div>
-							<label htmlFor="password" className="form-element">
-								Mot de passe
-							</label>
-							<div>
-								<a href="#">Mot de Passe oublié?</a>
-							</div>
-						</div>
-						<div>
-							<input
-								id="password"
-								name="password"
-								type="password"
-								autoComplete="current-password"
-								required
-							/>
-						</div>
-					</div>
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCredentials((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-					<div className="button-container">
-						<button type="submit">Connexion</button>
-					</div>
-				</form>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3001/api/v1/users", {
+        // Assurez-vous que l'URL est correcte pour POST
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
 
-				<p className="form-element">Vous n'êtes pas encore membre?</p>
-				<Link to="/inscription" className="form-insc">
-					Inscrivez-vous
-				</Link>
-			</div>
-		</div>
-	);
+      if (!response.ok) {
+        throw new Error(
+          "Échec de la connexion. Veuillez vérifier vos identifiants.",
+        );
+      }
+
+      const data = await response.json();
+      console.log("Login Success:", data);
+      localStorage.setItem("token", data.token); // Stocker le token JWT ici
+      if (data.userId) {
+        localStorage.setItem("userId", data.userId); // Stocker l'ID de l'utilisateur si disponible
+      }
+      navigate("/home"); // Rediriger vers la page d'accueil après une connexion réussie
+    } catch (error) {
+      console.error("Error:", error);
+      setError(error.message); // Affichage d'un message d'erreur sur l'interface utilisateur
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="form-wrapper">
+        <h2 className="form-element">Connectez-vous</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-element">
+            <label htmlFor="email">Adresse Mail</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              value={credentials.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-element">
+            <label htmlFor="password">Mot de passe</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              value={credentials.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="button-container">
+            <button type="submit">Connexion</button>
+          </div>
+        </form>
+        {error && <p className="error-message">{error}</p>}
+        <p className="form-element">Vous n'êtes pas encore membre?</p>
+        <Link to="/inscription" className="form-insc">
+          Inscrivez-vous
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ConnectionForm;
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,70 +1,132 @@
-import React from "react";
-import "../styles/UserComponent.css";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const RegistrationForm = () => {
-	return (
-		<div className="container">
-			<div className="form-wrapper">
-				<div>
-					<h2 className="form-element">Inscrivez-vous</h2>
-				</div>
-				<form action="#" method="POST">
-					<div className="form-element">
-						<label htmlFor="name">Nom</label>
-						<div>
-							<input
-								id="name"
-								name="name"
-								type="text"
-								autoComplete="name"
-								required
-							/>
-						</div>
-					</div>
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [successMessage, setSuccessMessage] = useState("");
 
-					<div className="form-element">
-						<label htmlFor="email">Adresse Mail</label>
-						<div>
-							<input
-								id="email"
-								name="email"
-								type="email"
-								autoComplete="email"
-								required
-							/>
-						</div>
-					</div>
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-					<div className="form-element">
-						<div>
-							<label htmlFor="password" className="form-element">
-								Mot de passe
-							</label>
-							<div>
-								<input
-									id="password"
-									name="password"
-									type="password"
-									autoComplete="new-password"
-									required
-								/>
-							</div>
-						</div>
-					</div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3001/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nickname: formData.nickname,
+        email: formData.email,
+        password: formData.password,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            "Network response was not ok: " + response.statusText,
+          );
+        }
+        return response.text();
+      })
+      .then((text) => {
+        try {
+          return JSON.parse(text);
+        } catch (err) {
+          throw new Error(
+            "Failed to parse JSON because the server didn't return JSON: " +
+              text,
+          );
+        }
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        setSuccessMessage(
+          "Inscription réussie! Vous pouvez maintenant vous connecter.",
+        );
+        setFormData({ name: "", email: "", password: "" }); // Réinitialiser les champs du formulaire
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Afficher un message d'erreur ici
+      });
+  };
 
-					<div className="button-container">
-						<button type="submit">Inscription</button>
-					</div>
-				</form>
+  return (
+    <div className="container">
+      <div className="form-wrapper">
+        <div>
+          <h2 className="form-element">Inscrivez-vous</h2>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-element">
+            <label htmlFor="name">Nom</label>
+            <div>
+              <input
+                id="name"
+                name="nickname"
+                type="text"
+                autoComplete="name"
+                required
+                onChange={handleChange}
+                value={formData.nickname}
+              />
+            </div>
+          </div>
 
-				<p className="form-element">Déjà membre?</p>
-				<Link to="/connection" className="form-insc">
-					Connectez-vous
-				</Link>
-			</div>
-		</div>
-	);
+          <div className="form-element">
+            <label htmlFor="email">Adresse Mail</label>
+            <div>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                onChange={handleChange}
+                value={formData.email}
+              />
+            </div>
+          </div>
+
+          <div className="form-element">
+            <label htmlFor="password">Mot de passe</label>
+            <div>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="new-password"
+                required
+                onChange={handleChange}
+                value={formData.password}
+              />
+            </div>
+          </div>
+
+          <div className="button-container">
+            <button type="submit">Inscription</button>
+          </div>
+        </form>
+        {successMessage && (
+          <p className="form-element success">{successMessage}</p>
+        )}
+        <p className="form-element">Déjà membre?</p>
+        <Link to="/connection" className="form-insc">
+          Connectez-vous
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default RegistrationForm;
